a2627706ffc658e510c507e4ce9c3c07a0df1b4f
Atharve Pandey <pandey.atharve@gmail.com>
2023-07-06 20:54:54 -0400

    initialized the git status

--- queryGenerator.java
+++ queryGenerator.java
@@ -9,7 +9,11 @@ public class queryGenerator {
 
         while (loop) {
             System.out.println("Welcome, what will you like to do?");
-            System.out.println("press A to add to the table\npress ");
+            System.out.println("press A to add something to the table\npress R to remove something from the table\npress Q to quit.");
+            Scanner scnr = new Scanner(System.in); 
+            String ans = scnr.next(); 
+            
+
         }
 
 
