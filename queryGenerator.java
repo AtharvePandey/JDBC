78bb1ce7d4e9077db1552ec68d46dc11df7f1456
Atharve Pandey <pandey.atharve@gmail.com>
2023-07-06 20:33:29 -0400

    gave this method its own .java file

---
+++ queryGenerator.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class queryGenerator {
+
+
+    public static String generateQuery(){
+        String query = "";
+        boolean loop = true;
+
+        while (loop) {
+            System.out.println("Welcome, what will you like to do?");
+            System.out.println("press A to add to the table\npress ");
+        }
+
+
+
+        return query; 
+    }
+
+
+}
